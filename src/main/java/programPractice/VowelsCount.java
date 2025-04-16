cf8a5f81b943fab41dd4901458af2fccf10cc4dd
KeerthanaVelkumaran <168756173+KeerthanaVelkumaran@users.noreply.github.com>
2025-04-16 18:11:09 +0530

    Update VowelsCount.java

--- src/main/java/programPractice/VowelsCount.java
+++ src/main/java/programPractice/VowelsCount.java
@@ -1,5 +1,14 @@
 package org.example;
+/**--Count the vowels in a string
 
+1. string can be anything like "i am a boy" or "Keerthana" or "this is the java programming .m/hd"
+
+approach would be
+1. vowels are {a,e,i,o,u} - in char array
+2. i need to traverse through the string through each char by char and loop for all the vowels
+3. first for loop would be each char of the string and second for loop would be vowels array
+4. first i will take the charAt(0) of the string and compare it to the each char of Vowels array
+	**/
 public class javaExample {
 	
 	static int count = 0;
