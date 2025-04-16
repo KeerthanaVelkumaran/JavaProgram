7fa4009ab174829e7499650bc0d3354743b1a68c
KeerthanaVelkumaran <168756173+KeerthanaVelkumaran@users.noreply.github.com>
2025-04-16 18:11:43 +0530

    Update VowelsCount.java

--- src/main/java/programPractice/VowelsCount.java
+++ src/main/java/programPractice/VowelsCount.java
@@ -24,10 +24,9 @@ public class javaExample {
 	public static int vowelsCount(String strString) {
 		
 		char[] chVow = {'a','e','i','o','u'};
-		System.out.println(strString.length());
-		System.out.println(chVow.length);
+		
 		 for(int i = 0; i<strString.length();i++){
-		 System.out.println(strString.charAt(i));
+		 
 			for(int j = 0; j<chVow.length;j++){
 				
 				if(strString.charAt(i)==chVow[j]){
